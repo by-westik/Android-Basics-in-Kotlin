601b07f5bd72ad94316f9e141d652ce0bf0ba421
by-westik <nastya.derevyanko.2020@mail.ru>
2023-03-28 17:49:21 +0300

    Add icons

--- app/src/main/java/com/by/westik/google/codelab/myapplication/MainActivity.kt
+++ app/src/main/java/com/by/westik/google/codelab/myapplication/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.by.westik.google.codelab.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.by.westik.google.codelab.myapplication.databinding.ActivityMainBinding
 import java.text.NumberFormat
 import kotlin.math.ceil
@@ -18,10 +22,14 @@ class MainActivity : AppCompatActivity() {
         binding.calculateButton.setOnClickListener {
             calculateTip()
         }
+
+        binding.costOfServiceEditText.setOnKeyListener {
+                view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
     }
 
     private fun calculateTip() {
-        val stringIntTextField = binding.costOfService.text.toString()
+        val stringIntTextField = binding.costOfServiceEditText.text.toString()
         val cost = stringIntTextField.toDoubleOrNull()
         if (cost == null) {
             binding.tipResult.text = ""
@@ -40,5 +48,14 @@ class MainActivity : AppCompatActivity() {
 
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
